b537fcfc99e4a61279abdfc676931a99db739ba1
paulovalves <paulovalves@outlook.com>
2024-03-22 14:56:53 -0300

    ajustes para padronizar com as tabelas

--- src/main/java/com/projeto/gerenciador/Models/Entities/Endereco.java
+++ src/main/java/com/projeto/gerenciador/Models/Entities/Endereco.java
@@ -3,23 +3,26 @@ package com.projeto.gerenciador.Models.Entities;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
+import org.hibernate.proxy.HibernateProxy;
 
-import java.util.UUID;
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "enderecos")
-public class Endereco {
+@Table(name = "enderecos", schema = "gerenciador")
+public class Endereco implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Id
     @Column(name = "id", nullable = false)
@@ -36,7 +39,7 @@ public class Endereco {
 
     @NotEmpty
     @NotBlank
-    @Column(name = "bairro", nullable = false)
+    @Column(name = "cep", nullable = false)
     private String cep;
 
     @NotEmpty
@@ -55,4 +58,20 @@ public class Endereco {
     @ManyToOne
     @JoinColumn(name = "pessoa_id", referencedColumnName = "id")
     private Pessoa pessoa;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Endereco endereco = (Endereco) o;
+        return getId() != null && Objects.equals(getId(), endereco.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
--- src/main/java/com/projeto/gerenciador/Models/Entities/Pessoa.java
+++ src/main/java/com/projeto/gerenciador/Models/Entities/Pessoa.java
@@ -7,17 +7,25 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.proxy.HibernateProxy;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "pessoas")
-public class Pessoa {
+@Table(name = "pessoas", schema = "gerenciador")
+public class Pessoa implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
@@ -34,6 +42,21 @@ public class Pessoa {
     private LocalDate dataNascimento;
 
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
-    @JoinColumn(name = "person_id")
     private List<Endereco> enderecos;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Pessoa pessoa = (Pessoa) o;
+        return getId() != null && Objects.equals(getId(), pessoa.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
