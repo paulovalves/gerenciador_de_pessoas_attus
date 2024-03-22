035113e667673a78929a0fce17458c44dd36bc3b
paulovalves <paulovalves@outlook.com>
2024-03-22 14:57:10 -0300

    scripts das tabelas

---
+++ src/main/java/com/projeto/gerenciador/database/tabelas.sql
@@ -0,0 +1,16 @@
+CREATE TABLE gerenciador.pessoas (
+                                     id SERIAL PRIMARY KEY,
+                                     nome_completo VARCHAR(255) NOT NULL,
+                                     data_nascimento DATE NOT NULL
+);
+
+CREATE TABLE gerenciador.enderecos (
+                                       id SERIAL PRIMARY KEY,
+                                       logradouro VARCHAR(255) NOT NULL,
+                                       numero VARCHAR(255),
+                                       cep VARCHAR(255) NOT NULL,
+                                       cidade VARCHAR(255) NOT NULL,
+                                       estado VARCHAR(255) NOT NULL,
+                                       principal BOOLEAN,
+                                       pessoa_id BIGINT REFERENCES gerenciador.pessoas(id)
+);
