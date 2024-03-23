ec8d1900b8b585c376552f8e744c9bb69a8f5e0a
paulovalves <paulovalves@outlook.com>
2024-03-23 18:37:35 -0300

    documentação do PessoaService

--- src/main/java/com/projeto/gerenciador/services/PessoaService.java
+++ src/main/java/com/projeto/gerenciador/services/PessoaService.java
@@ -7,25 +7,48 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
-import java.util.UUID;
 
+/**
+ * Classe de serviço para manipulação de entidades Pessoa.
+ */
 @Service
 public class PessoaService {
 
+    /**
+     * Repositório para acesso às entidades Pessoa no banco de dados.
+     */
     @Autowired
     private PessoaRepository pessoaRepository;
 
+    /**
+     * Serviço para manipulação de entidades Endereco.
+     */
     @Autowired
     private EnderecoService enderecoService;
 
+    /**
+     * Construtor para criar uma nova instância de PessoaService com o PessoaRepository e EnderecoService fornecidos.
+     *
+     * @param pessoaRepository O repositório para acesso às entidades Pessoa no banco de dados.
+     * @param enderecoService O serviço para manipulação de entidades Endereco.
+     */
     public PessoaService(PessoaRepository pessoaRepository, EnderecoService enderecoService) {
         this.pessoaRepository = pessoaRepository;
         this.enderecoService = enderecoService;
     }
 
+    /**
+     * Construtor padrão para criar uma nova instância de PessoaService.
+     */
     public PessoaService() {
     }
 
+    /**
+     * Recupera todas as entidades Pessoa do banco de dados.
+     *
+     * @return Uma lista de todas as entidades Pessoa.
+     * @throws RuntimeException se ocorrer algum erro durante o processo.
+     */
     public List<Pessoa> getPessoas() {
         try {
             return pessoaRepository.findAll();
@@ -34,6 +57,13 @@ public class PessoaService {
         }
     }
 
+    /**
+     * Recupera uma entidade Pessoa pelo seu ID.
+     *
+     * @param id O ID da entidade Pessoa a ser recuperada.
+     * @return A entidade Pessoa com o ID fornecido, ou null se tal entidade não existir.
+     * @throws RuntimeException se ocorrer algum erro durante o processo.
+     */
     public Pessoa getPessoaById(Long id) {
         try {
             return pessoaRepository.findById(id).orElse(null);
@@ -42,6 +72,13 @@ public class PessoaService {
         }
     }
 
+    /**
+     * Adiciona uma nova entidade Pessoa ao banco de dados.
+     *
+     * @param pessoa A entidade Pessoa a ser adicionada.
+     * @return A entidade Pessoa adicionada.
+     * @throws RuntimeException se ocorrer algum erro durante o processo.
+     */
     public Pessoa adicionarPessoa(@RequestBody Pessoa pessoa) {
         try {
             var endereco = pessoa.getEnderecos();
@@ -55,12 +92,8 @@ public class PessoaService {
             }
 
             return response;
-
-
         } catch (Exception e) {
             throw new RuntimeException("Erro ao adicionar pessoa");
         }
     }
-
-
 }
