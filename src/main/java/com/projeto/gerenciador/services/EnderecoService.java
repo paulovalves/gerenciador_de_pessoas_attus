1e1ec46a93b186f63a4f011b72d1b3d4e8272340
paulovalves <paulovalves@outlook.com>
2024-03-24 15:14:45 -0300

    adicionados metodos get ao EnderecoService

--- src/main/java/com/projeto/gerenciador/services/EnderecoService.java
+++ src/main/java/com/projeto/gerenciador/services/EnderecoService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional
 public class EnderecoService {
@@ -18,6 +20,31 @@ public class EnderecoService {
     }
 
     public EnderecoService() {
+    }
+
+
+    public List<Endereco> getTodosEnderecos() {
+        try {
+            return enderecoRepository.findAll();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao buscar todos os enderecos");
+        }
+    }
+
+    public List<Endereco> getEnderecoPorCep(String cep) {
+        try {
+            return enderecoRepository.findByCep(cep);
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao buscar endereco por cep");
+        }
+    }
+
+    public Endereco getEnderecoPorId(Long id) {
+        try {
+            return enderecoRepository.findById(id).get();
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao buscar endereco por id");
+        }
     }
 
     @Transactional
